4553d3c483cd58113b974214145d7e65be91683f
Toni-Ty <antonique2479@yahoo.com>
2019-09-01 04:19:03 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -5,4 +5,5 @@ class Character < ActiveRecord::Base
 end
 
 def say_that_thing_you_say
-  return "#{self.name} always says: #{self.catchphrase}"
+   "#{self.name} always says: #{self.catchphrase}"
+end
